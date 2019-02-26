20f631c4788d422288bacdffc889db6d2a06eb7e
kokhnovich <akokhnovich@yandex.ru>
2019-02-26 18:02:00 +0300

    do not work

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QGraphicsView>
+#include <QHBoxLayout>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    scene = new QGraphicsScene(this);
+
+    QBrush brush(QPixmap("/cell3.png"));
+    scene->setBackgroundBrush(brush);
+
+    ui->graphicsView->setScene(scene);
 }
 
 MainWindow::~MainWindow()
---
+++ src/view.h
@@ -0,0 +1,15 @@
+#ifndef VIEW_H
+#define VIEW_H
+
+
+class view
+{
+public:
+    view();
+
+private:
+    GraphicsView *graphicsView;
+
+};
+
+#endif // VIEW_H
